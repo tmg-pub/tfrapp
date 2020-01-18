77c1775190e1bded42784b2b9b445e04b92673cc
tmg-pub <tmg@clubtammy.info>
2020-01-18 02:44:28 -0600

    A script to setup the server side w/ python stuff.

---
+++ shebang.py
@@ -0,0 +1,21 @@
+# A script to ease the pain that is RUNNING PYTHON ON A LINUX SHARED SERVER
+# Usage: python3 shebang.py "path to PYTHON BINARY"
+# Also sets 0o755 permissions on all python files so the webserver can execute them.
+import glob, os
+import re
+import sys
+
+shebang = sys.argv[1]
+
+for file in glob.glob("*.py"):
+   if file == "shebang.py": continue
+   with open( file, "r" ) as fp:
+      content = fp.read()
+   content = re.sub( r"^#!.+?\n", "#!" + shebang + "\n", content )
+   with open( file, "w" ) as fp:
+      fp.write( content );
+   os.chmod( file, 0o755 )
+   print( file )
+   
+
+print( "done" )
