8e5da9538446f5bae48ea9e66c3d79c88abae9ee
tmg-pub <tmg@clubtammy.info>
2020-02-05 18:50:06 -0600

    Going to remove this.

--- getapp.py
+++ getapp.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
 # TFRAPP by Potter-MoonGuard (c) 2020
-# Testing sandbox.
 from __future__ import print_function
 from googleapiclient import errors
 import json
 import tfrapp as Me
 
+#------------------------------------------------------------------------------
+# This is to read the application content from the server to update the
+#  user's cache when using an edit link.
 def main():
-   Me.Put( """
-{
-      "poop": "pee"
-   }
-   """ )
+   input = Me.GetRequestBody()
+   db, dbc = Me.ConnectToDatabase()
+   dbc.execute(
+      "SELECT content FROM Apps WHERE editcode=%(editcode)s",
+      {
+         "editcode": input["editcode"]
+      }
+   )
+   result = dbc.fetchone()
+   
+   if result:
+      Me.Put({
+         "status": "OK",
+         "parts": json.loads(result[0])
+      })
+   else:
+      Me.Abort()
 
 #//////////////////////////////////////////////////////////////////////////////
 if __name__ == '__main__': Me.Run( main )
